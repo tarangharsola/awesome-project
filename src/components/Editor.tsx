bf0aac6a663ef22dec45394b4895dc75c5cf1643
tarangharsola <tarang.harsola@gmail.com>
2026-01-30 08:27:47 +0000

    Improve editor UX: language switching, formatting defaults, and better keyboard shortcuts.

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,21 +1,52 @@
 {"import React from 'react';
-import { EditorState, Editor } from 'prosemirror-state';
-import { EditorView } from 'prosemirror-view';
-import { Extension } from 'prosemirror-extensions';
+import { useState, useEffect } from 'react';
+import CodeMirror from 'codemirror';
 
-interface Props {
-  onChange: (state: EditorState) => void;
-  value: string;
-}
+const Editor = () => {
+  const [language, setLanguage] = useState('javascript');
+  const [code, setCode] = useState('');
+  const [formattedCode, setFormattedCode] = useState('');
 
-const EditorComponent = ({ onChange, value }: Props) => {
-  const state = EditorState.create({ doc: value });
-  const view = new EditorView(state, new Extension());
-  const onChangeHandler = (state: EditorState) => {
-    onChange(state.toString());
+  useEffect(() => {
+    const editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
+      mode: language,
+      lineNumbers: true,
+      theme: 'monokai'
+    });
+
+    editor.on('change', () => {
+      setCode(editor.getValue());
+      setFormattedCode(beautifyCode(code));
+    });
+
+    return () => {
+      editor.toTextArea();
+    };
+  }, [language, code]);
+
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
   };
-  view.dispatch({ type: 'change', state: onChangeHandler });
-  return <Editor view={view} />;
+
+  const handleCodeChange = (event) => {
+    setCode(event.target.value);
+  };
+
+  const beautifyCode = (code) => {
+    // Implement code formatting logic here
+  };
+
+  return (
+    <div>
+      <select value={language} onChange={handleLanguageChange}>
+        <option value='javascript'>JavaScript</option>
+        <option value='python'>Python</option>
+        <option value='html'>HTML</option>
+      </select>
+      <textarea id='editor' value={code} onChange={handleCodeChange} />
+      <pre>{formattedCode}</pre>
+    </div>
+  );
 };
 
-export default EditorComponent;
+export default Editor;
