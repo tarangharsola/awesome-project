1375fbdc85ce12df07b5c8927356f0cf720c7e62
tarangharsola <tarang.harsola@gmail.com>
2026-03-04 09:59:17 +0000

    BOT: Add a real code editor component with syntax highlighting for JavaScript, Python, and HTML.

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,1 +1,49 @@
-import React, { useState, useEffect } from 'react';import CodeMirror from 'codemirror';const Editor = ({ roomId }) => {  const [code, setCode] = useState('');  useEffect(() => {    const socket = io();    socket.on('code', (code) => {      setCode(code);    });  }, []);  return (    <div>      <CodeMirror value={code} onChange={(code) => {        socket.emit('code', code);      }} />    </div>  );};export default Editor;
+import CodeMirror from 'codemirror';
+import 'codemirror/addon/hint/show-hint';
+import 'codemirror/addon/hint/javascript-hint';
+import 'codemirror/addon/hint/html-hint';
+import 'codemirror/addon/hint/python-hint';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/matchonspace';
+import 'codemirror/addon/fold/foldcode';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/indent-fold';
+import 'codemirror/addon/search/matchesonscreen';
+import 'codemirror/addon/search/match-highlighter';
+import 'codemirror/addon/search/searchcursor';
+import 'codemirror/addon/dialog/dialog';
+import 'codemirror/addon/dialog/dialog-ext';
+import 'codemirror/addon/search/search';
+import 'codemirror/addon/hint/show-hint';
+import 'codemirror/addon/hint/javascript-hint';
+import 'codemirror/addon/hint/html-hint';
+import 'codemirror/addon/hint/python-hint';
+
+interface Props {
+  value: string;
+  onChange: (value: string) => void;
+  language: string;
+}
+
+const Editor = ({ value, onChange, language }: Props) => {
+  const cm = CodeMirror.fromTextArea(document.getElementById('editor')!, {
+    mode: language,
+    lineNumbers: true,
+    theme: 'monokai',
+    extraKeys: {
+      'Ctrl-Space': 'autocomplete',
+      'Ctrl-Shift-Space': 'show-hint',
+      'Shift-Tab': 'indentLess',
+      'Tab': 'indentMore',
+    },
+  });
+
+  cm.on('change', (instance, change) => {
+    onChange(instance.getValue());
+  });
+
+  return <div id="editor" style={{ height: '100vh' }}></div>;
+};
+
+export default Editor;
