18bca7344d5daa69a3faa7b23fe6082cc6afac08
Tarang Harsola <tarang.harsola@gmail.com>
2026-03-23 07:43:46 +0000

    BOT: Refactor for maintainability: split modules, improve types, and reduce complexity hotspots.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,20 @@
-{"import React from 'react';
-import ReconnectionHandler from './ReconnectionHandler';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Editor from './Editor';
+import UserList from './UserList';
+import Room from './Room';
+import LanguageSelector from './LanguageSelector';
 
 const App = () => {
   return (
-    <div>
-      <ReconnectionHandler />
-      <Editor />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Editor} />
+        <Route path="/users" component={UserList} />
+        <Route path="/rooms/:roomId" component={Room} />
+        <Route path="/languages" component={LanguageSelector} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
