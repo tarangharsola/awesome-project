2f33b1021c081aedf223261e2053513de71db989
Tarang Harsola <tarang.harsola@gmail.com>
2026-04-01 08:00:58 +0000

    BOT: Refactor for maintainability: split modules, improve types, and reduce complexity hotspots.

--- src/components/CursorTracker.tsx
+++ src/components/CursorTracker.tsx
@@ -1,27 +1,21 @@
-{"import React from 'react';
-import { useCursor } from '../useCursor';
+import React from 'react';
+import { useCursor } from './useCursor';
+import { useUsers } from './useUsers';
 
 const CursorTracker = () => {
-  const { cursor, users } = useCursor();
+   const cursor = useCursor();
+   const users = useUsers();
 
-  return (
-    <div className="cursor-tracker">
-      <h2>Cursors</h2>
-      <ul>
-        {users.map((user, index) => (
-          <li key={index} style={{
-            backgroundColor: user.color,
-            color: 'white',
-            padding: '5px',
-            borderRadius: '5px',
-            marginRight: '10px'
-          }}>
-            {user.name} ({cursor.position.x}, {cursor.position.y})
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+   return (
+      <div>
+         {users.map((user, index) => (
+            <div key={index}>
+               <span style={{ color: user.color }}>{user.name}</span>
+               <span> - Cursor position: {cursor.position}</span>
+            </div>
+         ))}
+      </div>
+   );
 };
 
 export default CursorTracker;
