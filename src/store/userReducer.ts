452762bd4406867aabac9c5506a5ec2ff58feb0d
Tarang Harsola <tarang.harsola@gmail.com>
2026-04-17 08:13:45 +0000

    BOT: Refactor for maintainability: split modules, improve types, and reduce complexity hotspots.

--- src/store/userReducer.ts
+++
@@ -1,6 +0,0 @@
-{"import { combineReducers } from 'redux';
-import { User } from './user';
-
-const userReducer = combineReducers({ user });
-
-export default userReducer;
--- src/store/usersReducer.ts
+++
@@ -1,6 +0,0 @@
-{"import { combineReducers } from 'redux';
-import { Users } from './users';
-
-const usersReducer = combineReducers({ users });
-
-export default usersReducer;
