7a3115a46bda7c52216ef5f02447515a6e2cb818
Tarang Harsola <tarang.harsola@gmail.com>
2026-04-22 08:14:34 +0000

    BOT: Improve active users panel and cursor labels (no neon/purple; readable on dark mode).

--- src/components/CursorTracker.tsx
+++ src/components/CursorTracker.tsx
@@ -1,11 +1,32 @@
 {"import React from 'react';
-import { useCursor } from './useCursor';
+import { useCursor } from '../useCursor';
 
-function CursorTracker() {
-  const cursor = useCursor();
+const CursorTracker = () => {
+  const { cursor, user } = useCursor();
+
   return (
-    <div style={{ position: 'absolute', top: cursor.y, left: cursor.x, backgroundColor: 'red', width: 2, height: 2 }} />
+    <div className="cursor-tracker">
+      <span style={{
+        backgroundColor: user.color,
+        color: "#fff",
+        padding: "5px",
+        borderRadius: "5px",
+        display: "inline-block",
+        margin: "5px"
+      }}>
+        {user.name}
+      </span>
+      <span style={{
+        position: "absolute",
+        top: cursor.y,
+        left: cursor.x,
+        width: "5px",
+        height: "5px",
+        backgroundColor: user.color,
+        borderRadius: "5px"
+      }} />
+    </div>
   );
-}
+};
 
 export default CursorTracker;
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,15 +1,34 @@
 {"import React from 'react';
-import { useUsers } from './useUsers';
+import { useState, useEffect } from 'react';
+import { useUsers } from '../useUsers';
 
-function UserList() {
-  const users = useUsers();
+const UserList = () => {
+  const { users, addUser, removeUser } = useUsers();
+  const [activeUsers, setActiveUsers] = useState([]);
+
+  useEffect(() => {
+    setActiveUsers(users.filter(user => user.isConnected));
+  }, [users]);
+
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <div className="active-users">
+      <h2>Active Users</h2>
+      <ul>
+        {activeUsers.map((user, index) => (
+          <li key={index} style={{
+            backgroundColor: user.color,
+            color: "#fff",
+            padding: "5px",
+            borderRadius: "5px",
+            display: "inline-block",
+            margin: "5px"
+          }}>
+            {user.name}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
 export default UserList;
