b21aeb3c585911e4bef777807cd492615596da13
Tarang Harsola <tarang.harsola@gmail.com>
2026-06-04 10:26:33 +0000

    BOT: Add connection status indicator and retry/backoff behavior.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,27 @@
 {"import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
-import Editor from './Editor';
+import { useState, useEffect } from 'react';
+import { useWebSocket } from './useWebSocket';
 
 function App() {
+  const [connectionStatus, setConnectionStatus] = useState('connected');
+  const { reconnect, isReconnecting } = useWebSocket();
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (isReconnecting) {
+        setConnectionStatus('reconnecting');
+      } else if (connectionStatus === 'reconnecting') {
+        setConnectionStatus('connected');
+      }
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [isReconnecting, connectionStatus]);
+
   return (
-    <Provider store={store}>
-      <Editor />
-    </Provider>
+    <div>
+      <h1>Connection Status: {connectionStatus}</h1>
+      <button onClick={reconnect}>Reconnect</button>
+    </div>
   );
 }
-
 export default App;
