028c7782df915e18884ac35ff01aeda118efd98d
Tarang Harsola <tarang.harsola@gmail.com>
2026-06-08 11:25:32 +0000

    BOT: Improve active users panel and cursor labels (no neon/purple; readable on dark mode).

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,15 +1,33 @@
 {"import React from 'react';
-import { useUsers } from '../utils/useUsers';
+import { User } from './User';
 
-const UserList = () => {
-  const users = useUsers();
-  return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
-  );
+interface UserListProps {
+  users: User[];
 }
 
+const UserList = ({ users }: UserListProps) => {
+  return (
+    <div className="user-list">
+      {users.map((user) => (
+        <User key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
 export default UserList;
+
+interface UserProps {
+  user: User;
+}
+
+const User = ({ user }: UserProps) => {
+  return (
+    <div className="user">
+      <span className="username">{user.username}</span>
+      <span className="color" style={{ backgroundColor: user.color }} />
+    </div>
+  );
+};
+
+export default User;
