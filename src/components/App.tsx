ec989752f0160c5845479f3280d081ba11af7876
Tarang Harsola <tarang.harsola@gmail.com>
2026-06-24 09:59:27 +0000

    BOT: Add connection status indicator and retry/backoff behavior.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,54 +1,23 @@
 {"import React from 'react';
 import { useState, useEffect } from 'react';
-import Editor from './Editor';
-import LanguageSelector from './LanguageSelector';
-import UserList from './UserList';
-import WebSocket from './WebSocket';
+import { useWebSocket } from './useWebSocket';
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [language, setLanguage] = useState('javascript');
-  const [code, setCode] = useState('');
-  const [cursorPositions, setCursorPositions] = useState({});
+  const [connectionStatus, setConnectionStatus] = useState('connected');
+  const { reconnect, connectionStatus: wsStatus } = useWebSocket();
 
   useEffect(() => {
-    const socket = new WebSocket('ws://localhost:8080');
-    socket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      if (data.type === 'updateUsers') {
-        setUsers(data.users);
-      } else if (data.type === 'updateCode') {
-        setCode(data.code);
-      } else if (data.type === 'updateCursorPositions') {
-        setCursorPositions(data.cursorPositions);
-      }
-    };
-    return () => {
-      socket.close();
-    };
-  }, []);
+    setConnectionStatus(wsStatus);
+  }, [wsStatus]);
 
-  const handleLanguageChange = (language) => {
-    setLanguage(language);
-  };
-
-  const handleCodeChange = (code) => {
-    setCode(code);
-  };
-
-  const handleCursorPositionChange = (userId, position) => {
-    setCursorPositions((prevCursorPositions) => ({
-      ...prevCursorPositions,
-      [userId]: position,
-    }));
+  const retry = () => {
+    reconnect();
   };
 
   return (
     <div>
-      <LanguageSelector language={language} onChange={handleLanguageChange} />
-      <Editor language={language} code={code} onChange={handleCodeChange} />
-      <UserList users={users} />
-      <WebSocket socket={socket} />
+      <h1>Connection Status: {connectionStatus}</h1>
+      <button onClick={retry}>Retry</button>
     </div>
   );
 }
