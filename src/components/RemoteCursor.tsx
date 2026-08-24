1e745b1b63b1486d41eac9f96f6c868871d9203b
Tarang Harsola <tarang.harsola@gmail.com>
2026-08-24 07:41:01 +0000

    BOT: Improve active users panel and cursor labels (no neon/purple; readable on dark mode).

--- src/components/RemoteCursor.tsx
+++ src/components/RemoteCursor.tsx
@@ -1,29 +1,42 @@
 import React from 'react';
-import { User } from '../types';
+import { CursorData } from '../types';
 
 type Props = {
-  user: User;
-  position: { top: number; left: number };
+  cursor: CursorData;
 };
 
-export const RemoteCursor: React.FC<Props> = ({ user, position }) => (
-  <div className="remote-cursor" style={{ top: position.top, left: position.left }}>
-    <div
-      className="cursor-label"
-      style={{
-        borderLeftColor: user.color,
-        backgroundColor: 'rgba(0, 0, 0, 0.6)',
-        color: '#fff'
-      }}
-    >
-      {user.name}
+export const RemoteCursor: React.FC<Props> = ({ cursor }) => {
+  const { position, color, name } = cursor;
+  const containerStyle: React.CSSProperties = {
+    position: 'absolute',
+    left: position.x,
+    top: position.y,
+    pointerEvents: 'none',
+    zIndex: 10,
+  };
+
+  const caretStyle: React.CSSProperties = {
+    width: '2px',
+    height: '1.2em',
+    backgroundColor: color,
+    display: 'inline-block',
+  };
+
+  const labelStyle: React.CSSProperties = {
+    backgroundColor: 'rgba(30, 30, 30, 0.8)',
+    color: '#fff',
+    padding: '2px 4px',
+    borderRadius: '3px',
+    fontSize: '0.75rem',
+    whiteSpace: 'nowrap',
+    marginTop: '-1.2em',
+    marginLeft: '4px',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <span style={caretStyle} />
+      <div style={labelStyle}>{name}</div>
     </div>
-    <div
-      className="cursor-dot"
-      style={{
-        backgroundColor: '#fff',
-        borderColor: user.color
-      }}
-    />
-  </div>
-);
+  );
+};
